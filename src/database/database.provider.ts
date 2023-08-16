ecf9260e0cdf3b485f79d35ba8c5d652b4c083d6
Faaiz <faaizaslam75@live.com>
2023-08-16 23:50:11 +0500

    PG fixed

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -7,9 +7,9 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'postgres',
         host: '127.0.0.1',
-        port: 3001,
-        username: 'root',
-        password: '',
+        port: 5432,
+        username: 'postgres',
+        password: 'admin',
         database: 'test',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
